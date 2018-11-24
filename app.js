1572ca26628f5ac3cbb089f8d888dae990ca3f4c
MedBenHadid <benhdid21@gmail.com>
2018-11-24 17:14:23 -0100

    2nd commit

--- app.js
+++ app.js
@@ -6,34 +6,55 @@ let ARG2 = process.argv[4]
 let F = fs.readFileSync("./note.json").toString()
 let T = JSON.parse(F)
 if (Comm=="list"){
- console.log("Printing "+T.length+" note(s).")
-    if(T.length!=0){
-        for (let i = 0; i < T.length; i++) {
-            console.log("-----------")
-            console.log("Title : " + T[i].title)
-            console.log("Body : " + T[i].body)
-        } 
-    }
-
+list(T)
 } else if (Comm=="add") {
-  
-    let T1={title :title,body :body}
-    T.push(T1)
-fs.writeFileSync("./note.json", JSON.stringify(T))
+  add(T,ARG1,ARG2)
 }else if(Comm=="read"){
    
-        if (ARG1 == "--title") {
-           
-            for (let i = 0; i < T.length; i++) {
-                if (T[i].title==ARG2) {
-                    console.log("Note Found------")
-                    console.log("Title : "+T[i].title)
-                    console.log("Body : "+T[i].body)
-                }              
-            }
-        }
+      read(ARG1,T)
 
 }else if(Comm=="remove"){
-  
- }
+remove(ARG1,T)
+}
+
+function list(T){
+console.log("Printing " + T.length + " note(s).")
+if (T.length != 0) {
+    for (let i = 0; i < T.length; i++) {
+        console.log("-----------")
+        console.log("Title : " + T[i].title)
+        console.log("Body : " + T[i].body)
+    }
+}
+}
+
+function add(T,ARG1,ARG2){
+   let T1 = {
+       title: ARG1,
+       body: ARG2
+   }
+   T.push(T1)
+   fs.writeFileSync("./note.json", JSON.stringify(T))
+   console.log("Note created !")
+}
+
+function read(ARG1,T){
+       if (ARG1 == "--title") {
+
+           for (let i = 0; i < T.length; i++) {
+               if (T[i].title == ARG2) {
+                   console.log("Note Found------")
+                   console.log("Title : " + T[i].title)
+                   console.log("Body : " + T[i].body)
+               }
+           }
+       }
+}
+
+function remove(ARG1,T){
+     if (ARG1 == "--title") {
+         const T1 = T.filter(item => item.title !== ARG2)
+         fs.writeFileSync("./note.json", JSON.stringify(T1))
+         console.log("Note was removed .")
+     }
 }
